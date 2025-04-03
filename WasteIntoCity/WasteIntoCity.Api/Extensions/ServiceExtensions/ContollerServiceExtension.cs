d0d9b11635fe616a7fb517b366e784dad74c9fbd
Ptuxa <sdnaft@gmail.com>
2025-04-03 21:41:41 +0300

    fix: added exception filter handling for incorrect format req

---
+++ WasteIntoCity/WasteIntoCity.Api/Extensions/ServiceExtensions/ContollerServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using WasteIntoCity.Api.Filters;
+
+namespace WasteIntoCity.Api.Extensions.ServiceExtensions
+{
+    public static class ContollerServiceExtension
+    {
+        public static IServiceCollection AddCustomControllers(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers(options =>
+            {
+                options.Filters.Add<RequestsValidationFilter>();
+            });
+
+            return services;
+        }
+    }
+}
---
+++ WasteIntoCity/WasteIntoCity.Api/Extensions/ServiceExtensions/ContollerServicesServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using WasteIntoCity.Application.Services;
+using WasteIntoCity.Core.Interfaces.Services;
+
+namespace WasteIntoCity.Api.Extensions.ServiceExtensions
+{
+    public static class ContollerServicesServiceExtension
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IIdentityService, IdentityService>();
+            services.AddScoped<IWorksService, WorksService>();
+            services.AddScoped<IWorkApplicationsService, WorkApplicationsService>();
+
+            return services;
+        }
+    }
+}
